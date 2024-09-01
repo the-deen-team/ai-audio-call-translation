4259666664c869a37545a75c4a3738ab11f2249e
YoAhdi <ahadahmad.work@gmail.com>
2024-08-31 22:47:20 -0700

    Clerk Authentication initial, will need more updates

--- app/sign-up/[[...sign-up...]]/page.js
+++ app/sign-up/[[...sign-up...]]/page.js
@@ -1,10 +1,8 @@
-import {ClerkProvider, SignedIn, SignedOut, SignIn} from "@clerk/nextjs";
+import {ClerkProvider, SignedIn, SignedOut, SignIn, SignUp } from "@clerk/nextjs";
 import {AppBar, Box, Button, Container, Toolbar, Typography} from '@mui/material'
 import Link from 'next/link'
 
-//Updated with clerk
-
-export default function SignInPage() {
+export default function SignUpPage() {
     return (
         <Container maxWidth="100vw">
             <AppBar position='static' sx= {{backgroundColor: "#3f51b5"}}>
@@ -12,7 +10,7 @@ export default function SignInPage() {
                     <Typography variant='h6' sx={{
                         flexGrow: 1,
                     }}>
-                        AI Audio Call Translator
+                        AI Call Translator
                     </Typography>
                     <Button color = "inherit" >
                         <Link href = "/sign-in" passHref>
@@ -33,29 +31,9 @@ export default function SignInPage() {
                 alignItems="center"
                 justifyContent="center"
             >
-            <Typography variant= "h4">Sign In</Typography>
-            <SignIn />
+            <Typography variant= "h4">Sign Up</Typography>
+            <SignUp />
             </Box>
         </Container>
     );
 }
-
-
-//Previous code 
-/*import { Box, Typography } from "@mui/material";
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      width="100vw"
-      height="100vh"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Typography variant="h3">AI Audio Call Translation</Typography>
-    </Box>
-  );
-}*/
