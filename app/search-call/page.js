7530bd6c539211513dcaa87160d3c290c07e639d
YoAhdi <ahadahmad.work@gmail.com>
2024-09-05 16:36:31 -0700

    List frontend initial

--- app/search-call/page.js
+++ app/search-call/page.js
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
-import {Typography, IconButton, Button } from "@mui/material";
+import { Typography } from "@mui/material";
+import Avatar from '@mui/joy/Avatar';
+import List from '@mui/joy/List';
+import ListDivider from '@mui/joy/ListDivider';
+import ListItem from '@mui/joy/ListItem';
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
 
 export default function FormPropsTextFields() {
   return (
@@ -10,25 +15,47 @@ export default function FormPropsTextFields() {
       sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
       noValidate
       autoComplete="off"
-
       display="flex"
       justifyContent="center"
       alignItems="center"
+      flexDirection="column"
       minHeight="100vh"
-      >
-
-
+    >
+      {/* Form Header and Search Field */}
       <div>
-      
-      
-      <Typography variant="h2">Start a Converstation</Typography>
-      <Typography variant="h4">Select add an individual you'd like to chat with</Typography>
+        <Typography variant="h2">Start a Conversation</Typography>
+        <Typography variant="h4">
+          Select and add an individual you'd like to chat with
+        </Typography>
+        <TextField id="outlined-search" label="Search User" type="search" />
+      </div>
 
-      <TextField id="outlined-search" 
-      label="Search User" 
-      type="search" />
-    </div>
+      {/* Avatar List */}
+      <div>
+        {['gutter', 'startDecorator'].map((inset) => (
+          <div key={inset}>
+            <Typography level="body-xs" sx={{ mb: 2 }}>
+              <code>{`placement="${inset}"`}</code>
+            </Typography>
+            <List variant="outlined" sx={{ minWidth: 240, borderRadius: 'sm' }}>
+              <ListItem>
+                <ListItemDecorator>
+                  <Avatar size="sm" src="https://via.placeholder.com/150" />
+                </ListItemDecorator>
+                Mabel Boyle
+              </ListItem>
+              <ListDivider inset={inset} />
+              <ListItem>
+                <ListItemDecorator>
+                  <Avatar size="sm" src="https://via.placeholder.com/150" />
+                </ListItemDecorator>
+                Boyd Burt
+              </ListItem>
+            </List>
+          </div>
+        ))}
+      </div>
     </Box>
-    
-);
+  );
 }
+
