1b6b941fa37a76d435b3f096acb22b461069fe96
YoAhdi <ahadahmad.work@gmail.com>
2024-09-05 00:11:36 -0700

    Conversation page Updated

--- app/search-call/page.js
+++ app/search-call/page.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import {Typography, IconButton, Button } from "@mui/material";
 
 export default function FormPropsTextFields() {
   return (
@@ -9,15 +10,25 @@ export default function FormPropsTextFields() {
       sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' } }}
       noValidate
       autoComplete="off"
+
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
       >
 
+
       <div>
+      
+      
+      <Typography variant="h2">Start a Converstation</Typography>
+      <Typography variant="h4">Select add an individual you'd like to chat with</Typography>
+
       <TextField id="outlined-search" 
       label="Search User" 
       type="search" />
     </div>
     </Box>
     
-
 );
 }
